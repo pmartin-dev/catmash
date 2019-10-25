18d75d9184cc287c5c906cc0f4cbdd323cb8a071
Pierre <pm4rtin@gmail.com>
2019-10-25 13:30:03 +0200

    ok pour affichage aleatoire des deux chats en HP à partir d'une liste dans le state, pas d'appel api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,76 @@
 import React,{Component} from 'react';
+import axios from 'axios'
+
+const API_URL = "https://latelier.co/data/cats.json"
 
 class Home extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+
+        this.state = {
+                images: [
+                    {
+                        "url": "http://24.media.tumblr.com/tumblr_m82woaL5AD1rro1o5o1_1280.jpg",
+                        "id": "MTgwODA3MA"
+                    },
+                    {
+                        "url": "http://24.media.tumblr.com/tumblr_m29a9d62C81r2rj8po1_500.jpg",
+                        "id": "tt"
+                    },
+                    {
+                        "url": "http://25.media.tumblr.com/tumblr_m4bgd9OXmw1qioo2oo1_500.jpg",
+                        "id": "bmp"
+                    }]
+            , 
+            chatgauche:{
+                "url":"",
+                "id":""
+            }, 
+            chatdroite:{
+                "url":"",
+                "id":""
+            }};
     }
+
+    UNSAFE_componentWillMount(){
+        this.initChats();
+    }
+
+    initChats(){
+        console.log(this.state.images)
+        // axios.get(`${API_URL}`,{headers:{crossDomain: true}}).then(response => {
+        //     // this.setState({chats:response.images})
+        //     console.log(response)
+        // }).catch(error => {console.log('error:',error)})
+
+        //choix chat gauche
+        const chats = [...this.state.images];
+        let choixgauche = Math.floor(Math.random() * Math.floor(chats.length));
+        let chatgauche = chats[choixgauche];
+
+        //choix chat droite
+        let choixdroite = Math.floor(Math.random() * Math.floor(chats.length));
+        let chatdroite = chats[choixdroite];
+
+        //on évite d'avoir le même chat à gauche et à droite
+        while(choixgauche === choixdroite){
+            choixdroite = Math.floor(Math.random() * Math.floor(chats.length));
+            chatdroite = chats[choixdroite];
+        }
+
+        // maj du state
+        this.setState({chatgauche, chatdroite});
+
+    }
+
+
 
     render(){
         return (
-            <h1>Home Page</h1>
+            <div className="presentationchats">
+                <div className="gauche"><img alt={this.state.chatgauche.id} src={this.state.chatgauche.url}></img></div>
+                <div className="droite"><img alt={this.state.chatdroite.id} src={this.state.chatdroite.url}></img></div>
+            </div>
         )
     }
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Menu = () => {
     return(
         <div>
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="/">CATMASH</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/">CATMASH</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <Link to="/" class="nav-link">Home<span class="sr-only">(current)</span></Link>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link">Home<span className="sr-only">(current)</span></Link>
                     </li>
-                    <li class="nav-item">
-                        <Link to="/score" class="nav-link">Scores</Link>
+                    <li className="nav-item">
+                        <Link to="/score" className="nav-link">Scores</Link>
                     </li>
                     </ul>
                 </div>
